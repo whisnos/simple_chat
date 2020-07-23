c0946e4c315b0954557549124f2ed78a0285100f
whisnos <whisnos@163.com>
2020-07-23 16:34:19 +0800

    dev

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # coding:utf-8
-
+import random
 import time
 import re
 import tornado.ioloop
@@ -12,7 +12,7 @@ import os
 import base64
 import tornado.log
 
-define('port',default=8080,type=int,help="监听端口")
+define('port',default=9999,type=int,help="监听端口")
 define('host',default="0.0.0.0",type=str,help="host")
 
 current_path = os.path.dirname(__file__)
@@ -46,7 +46,9 @@ class LoginHandler(RequestHandler):
 class ChatHandler(RequestHandler):
     def get(self, *args, **kwargs):
         ret = {}
-        ret['username'] = self.get_secure_cookie('username')
+        name = random.choice(the_list)
+        self.set_secure_cookie('username', name)
+        ret['username'] = name
         self.render('chat.html',**ret)
 
 class ChatWebSocketHandler(WebSocketHandler):
@@ -71,6 +73,7 @@ class ChatWebSocketHandler(WebSocketHandler):
         def on_close(self):
             users.remove(self)
 
+the_list = ["周杰伦","蔡依林","莫文蔚bai","冯德伦","萧亚轩du","徐若瑄","周渝民","吴建豪","朱孝天","孙楠","张柏芝zhi","谢霆锋","孙俪","邓超dao","马伊俐","文章","关之琳","吴佩慈","佟大为","关悦","那英","王菲","苏有朋","李亚鹏","周迅","古巨基","谭咏麟","巩俐","章子怡","范冰冰","李冰冰","李小璐","王珞丹","胡军","刘德华","黎明","郭富城","张学友","成龙","韩庚","汪东城","吴尊","陈乔恩","宋慧乔","何润东","蔡卓妍","刘亦菲","王力宏","梁静茹","王光良","罗大佑","周华健","李宗盛","齐秦","萧蔷","陈小春","陈晓东","应采儿","黄晓明","方力申","伍思凯","温兆伦","言承旭","陈浩民","立威廉","金城武","李连杰","孙红雷","蒲巴甲","罗中旭","韩红","陈红","徐帆","蒋雯丽","屠洪刚","濮存昕","许晴","林俊杰","王心凌","杜德伟","林子祥","谭耀文","张卫健","罗嘉良","费玉清","张雨生","徐静蕾","潘玮柏","陶喆","陈奕迅","范逸臣","方力申","周慧敏","曹格","苏永康","吴奇隆","曾志伟","曾宝仪","童安格","安以轩","杨丞琳","郑秀文","梁咏琪","胡杏儿","黄宗泽",]
 if __name__ == '__main__':
     options.parse_config_file('config.conf')
 
